12b9e36c9e8b32b8155d13fbb0f7582e7f6761e0
Sonia Boller <soniaboller@Sonias-MacBook-Pro.local>
2016-09-15 14:13:27 -0500

    added if else comparison to cards

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -11,4 +11,46 @@ var cardEight = "ace";
 var cardNine = "apple";
 var cardTen = "apple";
 var cardEleven = "banana";
-var cardTwelev = "banana";
+var cardTwelve = "banana";
+
+if (cardOne === cardTwo) {
+ 	alert("You found a match!");
+}
+ else {
+ 	alert("Sorry, try again");
+}
+
+if (cardThree === cardFour) {
+ 	alert("You found a match!");
+}
+ else {
+ 	alert("Sorry, try again");
+}
+
+if (cardFive === cardSix) {
+ 	alert("You found a match!");
+}
+ else {
+ 	alert("Sorry, try again");
+}
+
+if (cardSeven === cardEight) {
+ 	alert("You found a match!");
+}
+ else {
+ 	alert("Sorry, try again");
+}
+
+if (cardNine === cardTen) {
+ 	alert("You found a match!");
+}
+ else {
+ 	alert("Sorry, try again");
+}
+
+if (cardEleven === cardTwelve) {
+ 	alert("You found a match!");
+}
+ else {
+ 	alert("Sorry, try again");
+}
