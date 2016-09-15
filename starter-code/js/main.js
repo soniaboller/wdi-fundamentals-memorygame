ef34f91ba1143f69b5264eb8f5f69e3e3e1509f4
Sonia Boller <soniaboller@Sonias-MacBook-Pro.local>
2016-09-15 10:50:56 -0500

    created game logic

---
+++ starter-code/js/main.js
@@ -0,0 +1,14 @@
+console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+var cardFive = "cookie";
+var cardSix = "cookie";
+var cardSeven = "ace";
+var cardEight = "ace";
+var cardNine = "apple";
+var cardTen = "apple";
+var cardEleven = "banana";
+var cardTwelev = "banana";
